fd81f9507b82a2b5d388762d0408a2291a74a56c
sai kumar SKK <sai.korthivada@xcubelabs.com>
2019-03-22 01:03:15 +0530

    Implemented cards in course details page

--- src/app/views/admin-views/course-details/course-details.component.ts
+++ src/app/views/admin-views/course-details/course-details.component.ts
@@ -1,18 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+enum YEARS {
+  FIRST = 'firstYear',
+  SECOND = 'secondYear',
+  THIRD = 'thirdYear',
+  FOURTH = 'fourthYear'
+}
 @Component({
   selector: 'app-course-details',
   templateUrl: './course-details.component.html',
   styleUrls: ['./course-details.component.scss']
 })
+
 export class CourseDetailsComponent implements OnInit {
 
+  YEARS_ENUM = YEARS;
+  isActiveYears = {
+    firstYear: true,
+    secondYear: false,
+    thirdYear: false,
+    fourthYear: false
+  };
   constructor(private _router: Router) { }
 
   ngOnInit() {
   }
   navigateToDashboard() {
     this._router.navigate(['dashboard']);
+  }
+
+  yearChange(selectedYear) {
+    const keysOfYears = Object.keys(this.isActiveYears);
+    for (let i = 0; i < keysOfYears.length ; i++) {
+      if (selectedYear === keysOfYears[i]) {
+        this.isActiveYears[keysOfYears[i]] = true;
+      } else {
+        this.isActiveYears[keysOfYears[i]] = false;
+      }
+    }
   }
 }
