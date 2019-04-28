1beeb613faae43ba187f4e05df174c1c65c0d39f
sai kumar SKK <sai.korthivada@xcubelabs.com>
2019-04-28 09:12:15 +0530

    Created form array and save functionality

--- src/app/views/admin-views/course-subject-config/course-subject-config.module.ts
+++ src/app/views/admin-views/course-subject-config/course-subject-config.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { CourseSubjectConfigRoutingModule } from './course-subject-config-routing.module';
 import { CourseSubjectConfigComponent } from './course-subject-config.component';
+import { SharedModule } from '../../../shared/shared.module';
 
 @NgModule({
   declarations: [CourseSubjectConfigComponent],
   imports: [
     CommonModule,
-    CourseSubjectConfigRoutingModule
+    CourseSubjectConfigRoutingModule,
+    SharedModule
   ]
 })
 export class CourseSubjectConfigModule { }
